bc8b5ddb4a5bf4406d95d3b5763f7fd09da0fe8a
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-04-18 12:34:01 -0500

    Create new tool just for residential speed limits

--- script.js
+++ script.js
@@ -16,29 +16,4 @@ function computeTravelTime() {
     travelTime.value = result;
 }
 
-// Convert time to hours and minutes
-function timeConvert(num) {
-    let hours;
-    let minutes;
-    if (num === 1) {
-        return `1 hour`;
-    }
-    else if (num < 1) {
-        minutes = num * 60;
-        if (minutes === 1) {
-            return `1 minute`;
-        }
-        return `${minutes} minutes`;
-    }
-    else if (num > 1 && num < 2) {
-        hours = Math.floor(num);
-        minutes = (num - hours) * 60;
-        return `${hours} hour and ${minutes} minutes`
-    }
-    else {
-        hours = Math.floor(num);
-        minutes = (num - hours) * 60;
-        return `${hours} hours and ${minutes} minutes`
-    }
-}
 
