a8613df4b4bc37096846407e2ef69ab5cb342439
akcode2 <40044933+akcode2@users.noreply.github.com>
2023-04-18 13:14:24 -0500

    Complete functions to compute outputs

--- script.js
+++ script.js
@@ -3,17 +3,78 @@ const velocity = document.getElementById('velocity');
 const travelTime = document.getElementById('travelTime');
 const timeFormat = document.getElementById('timeFormat');
 
-velocity.oninput = computeTravelTime;
-distance.oninput = computeTravelTime;
+// Variables for residential speeding tool
+const resDistance = document.getElementById('resDistance');
+const resSpeedLimit = document.getElementById('resSpeedLimit');
+const resTime = document.getElementById('resTime');
+const resTimeIfBy5 = document.getElementById('resTimeIfBy5');
+const resTimeIfBy10 = document.getElementById('resTimeIfBy10');
+const resTimeIfBy15 = document.getElementById('resTimeIfBy15');
+const resDifferenceIfBy5 = document.getElementById('resDifferenceIfBy5');
+const resDifferenceIfBy10 = document.getElementById('resDifferenceIfBy10');
+const resDifferenceIfBy15 = document.getElementById('resDifferenceIfBy15');
+
+// Compute output using initial values
+computeResidentialTime();
+// Compute output when values are changed
+resDistance.oninput = computeResidentialTime;
+resSpeedLimit.oninput = computeResidentialTime;
 
 
-function computeTravelTime() {
-    // If either input is empty, do nothing
-    if (velocity.value === '' || distance.value === '') {
+function computeResidentialTime() {
+    if (resDistance.value === '' || resSpeedLimit.value === '') {
         return;
     }
-    result = timeConvert(parseInt(distance.value) / parseInt(velocity.value));
-    travelTime.value = result;
+    resTime.value = parseInt(resDistance.value) / parseInt(resSpeedLimit.value) * 60;
+
+    resTimeIfBy5.value = toTwoDecimals(parseInt(resDistance.value) / (parseInt(resSpeedLimit.value) + 5) * 60);
+    resDifferenceIfBy5.value = toTwoDecimals(resTime.value - resTimeIfBy5.value);
+    resTimeIfBy10.value = toTwoDecimals(parseInt(resDistance.value) / (parseInt(resSpeedLimit.value) + 10) * 60);
+    resDifferenceIfBy10.value = toTwoDecimals(resTime.value - resTimeIfBy10.value);
+    resTimeIfBy15.value = toTwoDecimals(parseInt(resDistance.value) / (parseInt(resSpeedLimit.value) + 15) * 60);
+    resDifferenceIfBy15.value = toTwoDecimals(resTime.value - resTimeIfBy15.value);
 }
 
+function toTwoDecimals(num) {
+    return Math.round(num * 100) / 100;
+}
+
+
+
+
+
+// function computeTravelTime(distance, velocity) {
+//     // If either input is empty, do nothing
+//     if (distance.value === '' || velocity.value === '') {
+//         return;
+//     }
+//     result = timeConvert(parseInt(distance.value) / parseInt(velocity.value));
+//     travelTime.value = result;
+// }
+
+// Convert time to hours and minutes
+// function timeConvert(num) {
+//     let hours;
+//     let minutes;
+//     if (num === 1) {
+//         return `1 hour`;
+//     }
+//     else if (num < 1) {
+//         minutes = num * 60;
+//         if (minutes === 1) {
+//             return `1 minute`;
+//         }
+//         return `${minutes} minutes`;
+//     }
+//     else if (num > 1 && num < 2) {
+//         hours = Math.floor(num);
+//         minutes = (num - hours) * 60;
+//         return `${hours} hour and ${minutes} minutes`
+//     }
+//     else {
+//         hours = Math.floor(num);
+//         minutes = (num - hours) * 60;
+//         return `${hours} hours and ${minutes} minutes`
+//     }
+// }
 
